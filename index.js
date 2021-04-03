998b525d0670bc39a5c9e323807036464518420f
angelina-tsuboi <angelina.t1832@gmail.com>
2021-04-03 17:54:31 +0900

    feat: added error detection

--- index.js
+++ index.js
@@ -17,6 +17,51 @@ bot.on('guildCreate', guild => {
   channel.send("Thanks for inviting me")
 })
 
+bot.on('error', (message, error) => {
+  switch (error) {
+      // Thrown when the YouTube search could not find any song with that query.
+      case 'SearchIsNull':
+          message.channel.send(`No song with that query was found.`);
+          break;
+      // Thrown when the provided YouTube Playlist could not be found.
+      case 'InvalidPlaylist':
+          message.channel.send(`No Playlist was found with that link.`);
+          break;
+      // Thrown when the provided Spotify Song could not be found.
+      case 'InvalidSpotify':
+          message.channel.send(`No Spotify Song was found with that link.`);
+          break;
+      // Thrown when the Guild Queue does not exist (no music is playing).
+      case 'QueueIsNull':
+          message.channel.send(`There is no music playing right now.`);
+          break;
+      // Thrown when the Members is not in a VoiceChannel.
+      case 'VoiceChannelTypeInvalid':
+          message.channel.send(`You need to be in a Voice Channel to play music.`);
+          break;
+      // Thrown when the current playing song was an live transmission (that is unsupported).
+      case 'LiveUnsupported':
+          message.channel.send(`We do not support YouTube Livestreams.`);
+          break;
+      // Thrown when the current playing song was unavailable.
+      case 'VideoUnavailable':
+          message.channel.send(`Something went wrong while playing the current song, skipping...`);
+          break;
+      // Thrown when provided argument was Not A Number.
+      case 'NotANumber':
+          message.channel.send(`The provided argument was Not A Number.`);
+          break;
+      // Thrown when the first method argument was not a Discord Message object.
+      case 'MessageTypeInvalid':
+          message.channel.send(`The Message object was not provided.`);
+          break;
+      // Thrown when the Guild Queue does not exist (no music is playing).
+      default:
+          message.channel.send(`**Unknown Error Ocurred:** ${error}`);
+          break;
+  }
+});
+
 bot.on("ready", () => {
   console.log("I am ready to Play with DMP 🎶");
 });
@@ -46,6 +91,7 @@ bot.on('message', async (message) => {
     console.log("playing");
     let song = await bot.player.play(message, args.join(' '));
         
+  
     // If there were no errors the Player#songAdd event will fire and the song will not be null.
     if(song)
         console.log(`Started playing ${song.name}`);
