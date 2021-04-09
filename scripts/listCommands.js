6538bb67563c6e45a6cf7e003de94bb0f3aa2fa7
angelina-tsuboi <angelina.t1832@gmail.com>
2021-04-09 18:15:38 +0900

    feat: moved index.html to root

--- scripts/listCommands.js
+++ scripts/listCommands.js
@@ -46,7 +46,7 @@ export function listHelpSettings(){
 	.setColor('#0099ff')
 	.setTitle('Ambience Help')
 	.setURL('https://discord.js.org/')
-	.setDescription('📄 Click Here to View All Commands \n\n 🛠 Need Help? Please visit our Troubleshooting page. \n\n 🌌 New to Ambience? Join our community');
+	.setDescription('📄 [Click Here](https://countrycode.org/) to View All Commands \n\n 🛠 Need Help? Please visit our Troubleshooting page. \n\n 🌌 New to Ambience? Join our community');
     return helpEmbed;
 }
 
